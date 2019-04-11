0ff87e8e6c221ce7bdb4fe0623395df0a9e22dd7
Vinodh Thiagarajan <VThiagarajan@ITs-MacBook-Pro.local>
2019-04-11 16:40:35 -0500

    First commit :

---
+++ src/main/java/com/intersys/successmanager/SuccessManagerApplication.java
@@ -0,0 +1,13 @@
+package com.intersys.successmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccessManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccessManagerApplication.class, args);
+	}
+
+}
